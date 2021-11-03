16b94c259348b4599ceada0b3f412e87097c347e
PhamNguyett <ptnguyetttt@gmail.com>
2021-11-03 14:42:51 +0700

    A

---
+++ assets/javascript/main.js
@@ -0,0 +1,29 @@
+const buyBtns= document.querySelectorAll('.js-buy-ticket')
+const modal = document.querySelector('.js-modal')
+const modalClose= document.querySelector('.js-modal-close')
+const modalContainer=document.querySelector('.js-modal-container')
+
+//Hàm hiển thị modal mua vé (thêm class open vào modal)
+function showBuyTicket()
+{
+    modal.classList.add('open')
+}
+
+//Lặp qua từng thẻ button và nghe hành vi click
+for(const buyBtn of buyBtns){
+    buyBtn.addEventListener('click',showBuyTicket)
+}
+
+//Hàm ẩn modal mua vé (gỡ bỏ class open của modal)
+function closeBuyTicket()
+{
+    modal.classList.remove('open')
+}
+
+//Nghe hành vi click 
+modalClose.addEventListener('click',closeBuyTicket)
+modal.addEventListener('click',closeBuyTicket)
+modalContainer.addEventListener('click',function(event)
+{
+    event.stopPropagation()
+})
